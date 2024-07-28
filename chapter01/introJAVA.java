2a7e5ce649319dc60e635475c8c7846d658fb368
EunYeol <163628847+giteunyeol@users.noreply.github.com>
2024-07-28 21:34:16 +0900

    Add files via upload

---
+++ chapter01/introJAVA.java
@@ -0,0 +1,43 @@
+package chapter01;
+
+public class introJAVA {
+	// 1. JAVA 버전
+	//  - SE(Standard Edition) : 기본과
+	//  - EE(Enterprise Edition) : (Web, 기업용)
+	//  - ME(Micro Edition) : 초소형 가전용 -> Android로 대체
+	
+	// 2. JAVA 특성
+	// - 캡슐화(은닉성) : 객체의 구조를 외부로부터 숨김(보안 향상) -> Getter & Setter
+	// - OS(운영체제 자유로움)
+	//    -> 4대 운영체제(Windows, MacOs, Linux, Unix)
+	//    특정 언어들은 운영체제별로 개발을 다르게 해야함!
+	//    동일한 프로그램을 개발해도 운영체제별로 4개를 개발해야함
+	//    -> JVM(Java Virtual Machine)을 통해서 가능!
+	//     * JVM: JAVA 프로그램을 실행할 때 반드시 필요한 머신
+	//     -> 개발코드는 동일: Linux JVM, Unix JVM       --jre에 들어있음
+	// - 메모리 관리 안해도 됨!
+	//   - C언어의 경우 메모리 관리를 개발자가 직접해줘야함
+	//   - JAVA는 메모리관리 없이 Garbage Collector가 알아서 처리해 줌
+	
+	// 3. JAVA 코드 실행 과정 --- *시험*
+	//  - (파일명).java 파일 생성 = 원시파일
+	//  -      
+	//  - JAVAC 컴파일러    -(ctrl s)
+	//  -
+	//  - (파일명).class 파일 생성 = Byte-code)
+	//  -
+	//  - Java (파일명) 실행
+	//  - 
+	//  - JVM(인터프리터) -> 실행결과 출력
+	
+	// 4. JAVA 프로젝트 구조
+	//   - 프로젝트
+	//        ㄴ패키지 1
+	//           ㄴ클래스1
+	//        ㄴ패키지 2
+	//           ㄴ클래스2
+	//           ㄴ클래스3
+	
+	//
+	
+}
